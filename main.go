f9cbd244ae36607dbd0ae953454e45fab8e9d3dd
xMrDeathx <kuznik15@mail.ru>
2024-12-01 19:28:48 +0300

    Added config
    Added file with environment configuration and implemented import from it

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "TestWork/di"
+import (
+	"TestWork/di"
+	"github.com/joho/godotenv"
+	"log"
+)
 
 func main() {
+	err := godotenv.Load("config.env")
+	if err != nil {
+		log.Panic("Ошибка при загрузке .env файла", err)
+	}
+
 	di.Migrate()
 	di.InitAppModule()
 }
